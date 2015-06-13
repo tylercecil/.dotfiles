3e28e92f80f7d811f45b8cd4ae38151146ec9415
Izzy Cecil <lorr.cecil@gmail.com>
2015-06-13 12:02:05 -0600

    Emacs: markdown mode.

--- emacs.d/config/modes/markdown.el
+++ emacs.d/config/modes/markdown.el
@@ -1,7 +1,11 @@
 (use-package markdown-mode
   :ensure t
-  :mode ("\\.md\\'" . markdown-mode))
+  :mode ("\\.md\\'" . markdown-mode)
+  :config
+  (message "stage 1"))
 
-(use-package gh-md
-  :ensure t
-  :mode ("\\.md\\'" . markdown-mode))
+;(use-package gh-md
+;  :ensure t
+;  :mode ("\\.md\\'" . markdown-mode)
+;  :config
+;  (message "stage 2"))
