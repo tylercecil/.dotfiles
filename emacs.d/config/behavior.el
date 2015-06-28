284e16d7f48b5715a63cb390a33d9b033af6a02d
Izzy Cecil <lorr.cecil@gmail.com>
2015-06-28 11:07:09 -0700

    Emacs: seperate style and theme file.

--- emacs.d/config/behavior.el
+++ emacs.d/config/behavior.el
@@ -24,15 +24,7 @@
 ;;
 (electric-pair-mode 1)
 (add-to-list 'electric-pair-pairs '(?\{ . ?\}))
-(add-to-list 'electric-pair-pairs '(?\{ . ?\}))
 
-;;
-;; Rainbow Delims
-;;
-(use-package rainbow-delimiters
-  :ensure t
-  :diminish rainbow-delimiters-mode
-  :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 ;;
 ;; Spell check (we all know I need it!)
@@ -104,82 +96,3 @@
 
 ;; Always reload files when changed on disk
 (global-auto-revert-mode t)
-
-;;
-;; y-or-n over yes-or-no
-;;
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;;
-;; Code Style
-;;
-;; Use spaces instead of tabs
-(setq-default indent-tabs-mode nil)
-;; Set tab to display as 4 spaces
-(setq-default tab-width 4)
-;; Set stop-tabs to be 4 written as spaces
-(setq-default tab-stop-list (number-sequence 4 120 4))
-
-;;
-;; Theme things
-;;
-(use-package highlight-chars
- :ensure t
- :init
- (add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
- (add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
- :config
- (hc-highlight-tabs))
-
-(use-package fill-column-indicator
- :ensure t
- :init
- (add-hook 'font-lock-mode-hook (lambda () (fci-mode 80)))
- :config
- (setq fci-rule-column 80))
-
-;; Don't use messages that you don't read
-(setq initial-scratch-message "")
-(setq inhibit-startup-message t)
-
-;; Don't let Emacs hurt your ears
-(setq ring-bell-function 'ignore)
-
-;; Highlight line
-(global-hl-line-mode)
-
-;; Column numbers, please
-(column-number-mode)
-
-;; Linnum mode
-(global-linum-mode 1)
-
-;; Sets a gutter between linnums and text
-(unless window-system
-  (add-hook 'linum-before-numbering-hook
-            (lambda ()
-              (setq-local linum-format-fmt
-                          (let ((w (length (number-to-string
-                                            (count-lines (point-min) (point-max))))))
-                            (concat "%" (number-to-string w) "d"))))))
-
-(defun linum-format-func (line)
-  (concat
-   (propertize (format linum-format-fmt line) 'face 'linum)
-   (propertize " " 'face 'mode-line)))
-
-(unless window-system
-    (setq linum-format 'linum-format-func))
-
-;; Theme in X
-(when (display-graphic-p)
-  ;; Transparency
-  ;; (add-to-list 'default-frame-alist '(alpha 90 90))
-  ;; No Scrollbar
-  (scroll-bar-mode -1)
-  ;; No Toolbar
-  (tool-bar-mode -1)
-  ;; No menu bar
-  (menu-bar-mode -99)
-  ;; Font
-  (set-frame-font "Anonymous Pro 14"))
---
+++ emacs.d/config/theme.el
@@ -0,0 +1,72 @@
+;;
+;; Rainbow Delims
+;;
+(use-package rainbow-delimiters
+  :ensure t
+  :diminish rainbow-delimiters-mode
+  :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+
+;;
+;; y-or-n over yes-or-no
+;;
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Sets a gutter between linnums and text
+(unless window-system
+  (add-hook 'linum-before-numbering-hook
+            (lambda ()
+              (setq-local linum-format-fmt
+                          (let ((w (length (number-to-string
+                                            (count-lines (point-min) (point-max))))))
+                            (concat "%" (number-to-string w) "d"))))))
+
+(defun linum-format-func (line)
+  (concat
+   (propertize (format linum-format-fmt line) 'face 'linum)
+   (propertize " " 'face 'mode-line)))
+
+(unless window-system
+    (setq linum-format 'linum-format-func))
+
+(use-package highlight-chars
+ :ensure t
+ :init
+ (add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
+ (add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
+ :config
+ (hc-highlight-tabs))
+
+;; Don't use messages that you don't read
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+
+;; Don't let Emacs hurt your ears
+(setq ring-bell-function 'ignore)
+
+;; Highlight line
+(global-hl-line-mode)
+
+;; Column numbers, please
+(column-number-mode)
+
+;; Linnum mode
+(global-linum-mode 1)
+
+;; No line-wrap
+(set-default 'truncate-lines t)
+
+;; Theme in X
+(when (display-graphic-p)
+  (use-package exec-path-from-shell
+    :ensure t
+    :config (exec-path-from-shell-initialize))
+  ;; Transparency
+  ;; (add-to-list 'default-frame-alist '(alpha 90 90))
+  ;; No Scrollbar
+  (scroll-bar-mode -1)
+  ;; No Toolbar
+  (tool-bar-mode -1)
+  ;; No menu bar
+  (menu-bar-mode -99)
+  ;; Font
+  (set-frame-font "Anonymous Pro 14"))
