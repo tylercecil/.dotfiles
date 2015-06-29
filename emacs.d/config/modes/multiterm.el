6da48bc9c525567d09dfa9eb852ac282119e2801
Izzy Cecil <lorr.cecil@gmail.com>
2015-06-29 09:09:27 -0600

    Emacs: Term mode tab completion.

--- emacs.d/config/modes/multiterm.el
+++ emacs.d/config/modes/multiterm.el
@@ -1,3 +1,8 @@
 (use-package multi-term
   :ensure t
+  ;; This fixes a bug with yas-snippets that breaks tab completion in
+  ;; any term mode.
+  ;; See https://github.com/capitaomorte/yasnippet/issues/289
+  :init (add-hook 'term-mode-hook (lambda()
+                                    (yas-minor-mode -1)))
   :config (setq multi-term-program "/usr/local/bin/zsh"))
