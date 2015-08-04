ed3d8764c8748265f3764e49114803ee2b03344a
Izzy Cecil <lorr.cecil@gmail.com>
2015-08-04 11:23:38 -0600

    Emacs: A working latex mode?

---
+++ emacs.d/config/modes/latex.el
@@ -0,0 +1,22 @@
+(use-package tex-site
+  :ensure auctex
+  ;; :mode ("\\.tex\\'" . auctex-mode)
+  :commands (LaTeX-mode plain-tex-mode)
+  :config
+  (TeX-fold-mode 1)
+  (outline-minor-mode 1)
+
+  ;; Mathmode and reftex
+  (add-hook 'TeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'TeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX t)
+
+  ;; File saving things
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil)
+
+  ;; LuaTeX stuff
+  (setq-default TeX-PDF-mode t)
+  (setq-default TeX-engine 'luatex)
+  (setq-default TeX-command-extra-options "-shell-escape"))
