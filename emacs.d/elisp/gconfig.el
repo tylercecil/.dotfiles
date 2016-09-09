d1b8a1ecc6651e9cbefa66959bbff71b536e2d81
Tyler Cecil <tyler.i.cecil@gmail.com>
2016-09-09 11:20:45 -0700

    Some basic google settings for emacs... will continue to add.

--- emacs.d/elisp/gconfig.el
+++ emacs.d/elisp/gconfig.el
@@ -1,4 +1,34 @@
 (defun gconfig-init ()
-  (use-package google))
+  (use-package google
+    :config
+
+    (defun toggle-g3-coverage ()
+      "Either run google3-display-coverage, or googl3-display-coverage-clear."
+      (interactive)
+      (if (null google3-display-coverage--overlays)
+          (google3-display-coverage)
+        (google3-display-coverage-clear)
+        (message "Coverage cleared")))
+    ;; Need to default overlays to nil
+    (setq google3-display-coverage--overlays nil)
+
+    (defun prompt-autogen-hook ()
+      "Call google-autogen-file-not-found-hook only if the user gives the ok."
+      ;; I don't like dialog boxes.
+      ;; See Yes-or-No Queries in the emacs manual.
+      (setq last-nonmenu-event "no box, please!")
+      (when (y-or-n-p "File does not exist. Should it be google-autogend?")
+        (autogen-file-not-found)))
+    (add-hook 'find-file-not-found-functions #'prompt-autogen-hook)
+
+    ;; Will use "g" for a google prefix... this may one day conflict with a
+    ;; concept of "going", but I suspect I will stick with autojump forever.
+    (evil-leader/set-key
+      "gt" 'toggle-g3-coverage
+      "ga" 'autogen
+      "gb" 'google3-build
+      "gll" 'google-lint
+      "gln" 'next-error
+      "glp" (lambda () (interactive) (next-error -1)))))
 
 (provide 'gconfig)
