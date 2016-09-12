b3c701b70bf9990675428ce6017f384c8c9f1716
Tyler Cecil <tyler.i.cecil@gmail.com>
2016-09-12 12:43:49 -0700

    Some zsh things for google.

--- zshplugins/google/google.plugin.zsh
+++ zshplugins/google/google.plugin.zsh
@@ -3,3 +3,8 @@
 
 # This is needed for g4d
 source /etc/bash_completion.d/g4d
+source /etc/bash_completion.d/p4
+
+alias haste='/google/src/head/depot/eng/tools/pastebin'
+
+export G3='/google/src/head/depot/google3/'
