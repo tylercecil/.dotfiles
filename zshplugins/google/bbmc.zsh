56f8c3a860bd3625d2dacaab74d9fa2109fb7758
Tyler Cecil <tcecil@google.com>
2016-10-11 11:52:05 -0700

    some bbmc utils.

--- zshplugins/google/bbmc.zsh
+++ zshplugins/google/bbmc.zsh
@@ -1,13 +1,30 @@
 # I always build the same kind of VM
 alias findvm='gcloud compute instances list | ag tcecil'
-alias rmvm='gcloud compute instances delete tcecil-test'
-alias sshvm='export TERM=xterm; gcloud compute ssh tcecil-test'
+alias rmvm='gcloud compute instances delete '
+alias rmvmall='rmvm `findvm | cut -d " " -f 1` '
+alias sshvm='export TERM=xterm; gcloud compute ssh '
+
+function getserial {
+  gcloud compute instances get-serial-port-output $1 --port=2 --zone='us-central1-ir1'
+}
 
 function mkvm {
-  local cfg=${1:-~/workspace/bbmc/google3/net/fabric/monitoring/bbmc/tools/my-cloud-config.yaml}
-  gcloud compute instances create tcecil-test                               \
-         --image-project bct-staging-images --image gci-stable-53-8530-85-0 \
-         --metadata-from-file user-data=$cfg
+  local cfg=~/workspace/bbmc/google3/net/fabric/monitoring/bbmc/tools/my-cloud-config.yaml
+  if [[ $1 == '-c' ]]; then
+    cfg=$2
+    shift 2
+  fi
+  cfg="--metadata-from-file=user-data=$cfg"
+  local image=$(gcloud compute images list --project bct-staging-images  --no-standard-images \
+                  | ag gci | tail -n 1 | cut -d ' ' -f 1)
+
+  echo "Building $# VMs with the following configuration: $cfg"
+  echo "Using image $image"
+
+  for vm in $*; do
+    gcloud compute instances create $vm \
+           --image-project bct-staging-images --image $image $cfg
+  done
 }
 
 function bbmcpush {
