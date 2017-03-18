30aa774d9a6dd6ee7bccc67ebb2a9619d2cca122
Tyler Cecil <tyler.i.cecil@gmail.com>
2017-03-18 15:13:50 -0700

    emacs: agda-config.el adds some vim keys

--- emacs.d/modes/agda-config.el
+++ emacs.d/modes/agda-config.el
@@ -1,8 +1,39 @@
-;; Agda mode is not in melpa, but is an actual program that needs
-;; to be run to generate it.
-;; TODO: Clean this up with an autoload of some kind
+;; Agda mode is not in melpa, but is an actual program that needs to be run to
+;; generate it.
 (load-file (let ((coding-system-for-read 'utf-8))
              (shell-command-to-string "agda-mode locate")))
+
+;; Key configuration for evil
+(evil-leader/set-key-for-mode 'agda-mode
+  "mbl" `agda2-load
+  "mbb" `agda2-compile
+  "mbr" `agda2-compute-normalised-maybe-toplevel
+  "mqq" `agda2-quit
+  "mqr" `agda2-restart
+
+  "ms" `agda2-search-about-toplevel
+
+  "g." `agda2-goto-definition-keyboard
+
+  ;; Goals
+  "mn" `agda2-next-goal
+  "mp" `agda2-previous-goal
+  "mr" `agda2-refine
+  "ma" `agda2-auto
+  "mc" `agda2-make-case
+  "mt" `agda2-goal-type
+  "mi" `agda2-goal-and-context-and-inferred
+
+  ;; `agda2-give
+  ;; `agda2-show-context
+  ;; `agda2-infer-type
+  ;; `agda2-module-contents
+  ;; `agda2-remove-annotations
+  ;; `agda2-display-implicit-arguments
+  ;; `agda2-show-constraints
+  ;; `agda2-show-goals
+  )
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
