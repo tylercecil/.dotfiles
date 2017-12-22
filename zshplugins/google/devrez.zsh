9a6d7c09f2a2cf83889d24ea3a5fb2e704f3640b
Tyler Cecil <tcecil@google.com>
2017-12-21 16:03:43 -0800

    Thinned out google plugin for zsh.

--- zshplugins/google/devrez.zsh
+++
@@ -1,7 +0,0 @@
-function devrez {
-  local com=$1
-  shift
-  # blaze run //net/fabric/testing/e2e/utils/devrez:devrez_andromeda -- \
-  blaze run //net/fabric/testing/e2e/utils:devrez_andromeda -- \
-        --command=$com $@
-}
--- zshplugins/google/google.plugin.zsh
+++ zshplugins/google/google.plugin.zsh
@@ -1,6 +1,4 @@
 # GOOGLE SPECIFIC UTILITIES
-# If anything here develops to a point of complexity, it can be added as a sub-plugin.
-
 # This is needed for g4d
 source /etc/bash_completion.d/g4d
 source /etc/bash_completion.d/p4
@@ -8,6 +6,4 @@ source /etc/bash_completion.d/p4
 alias haste='/google/src/head/depot/eng/tools/pastebin'
 export G3='/google/src/head/depot/google3/'
 
-source /usr/local/google/home/tcecil/.dotfiles/zshplugins/google/sandman.zsh
-source /usr/local/google/home/tcecil/.dotfiles/zshplugins/google/devrez.zsh
-source /usr/local/google/home/tcecil/.dotfiles/zshplugins/google/bbmc.zsh
+source /usr/local/google/home/tcecil/.dotfiles/zshplugins/google/gcloud.zsh
