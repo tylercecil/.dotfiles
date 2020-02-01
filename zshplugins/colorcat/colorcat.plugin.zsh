257db73f00d67d6d7925663e8f7dbaee04b8e6e8
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-01-31 22:22:40 -0600

    kak editor?

--- zshplugins/colorcat/colorcat.plugin.zsh
+++ zshplugins/colorcat/colorcat.plugin.zsh
@@ -1,3 +1,4 @@
 # Colored Readers
 alias ccat='pygmentize -g'
 export LESSOPEN='| pygmentize -g %s'
+alias less='kak -ro'
--- zshplugins/editor/editor.plugin.zsh
+++ zshplugins/editor/editor.plugin.zsh
@@ -1,4 +1,4 @@
-export EDITOR=nvim
+export EDITOR=kak
 export ALTERNATE_EDITOR=''
 
 alias e=$EDITOR
