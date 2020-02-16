62f97c29aff7dcd8b0b64c07b2608dd7e282f507
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-02-15 20:13:29 -0600

    remove sorting of applications, because... why?

--- bin/fzf_launch.sh
+++ bin/fzf_launch.sh
@@ -4,8 +4,7 @@ CMD=$(echo -n "$PATH" | \
   xargs -d: -I{} -r -- \
   find -L {} -maxdepth 1 -mindepth 1 -type f -executable -printf '%P\n' \
   2>/dev/null | \
-  sort -u | \
-  fzf --reverse --border --color=bw)
+  fzf --reverse --border --color=light)
 
 # I've tried this, and `$CMD & disown`. Same result.
 # $CMD & disown
