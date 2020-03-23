90f0f89564e52507c95ceab668f52944eb66b9ad
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-03-23 10:28:35 -0500

    back to using neovim.

--- zshplugins/colorcat/colorcat.plugin.zsh
+++ zshplugins/colorcat/colorcat.plugin.zsh
@@ -1,4 +1,3 @@
 # Colored Readers
 alias ccat='pygmentize -g'
 export LESSOPEN='| pygmentize -g %s'
-alias less='kak -ro'
--- zshplugins/editor/editor.plugin.zsh
+++ zshplugins/editor/editor.plugin.zsh
@@ -1,4 +1,4 @@
-export EDITOR=kak
+export EDITOR=nvim
 export ALTERNATE_EDITOR=''
 
 alias e=$EDITOR
