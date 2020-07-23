1512e8ab1a0416086455b0dd2b139d93f6a1063a
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-07-23 08:56:03 -0700

    emacs:zsh: changes "en" alias to a function, allowing for arguments to be passed.

--- zshplugins/editor/editor.plugin.zsh
+++ zshplugins/editor/editor.plugin.zsh
@@ -3,5 +3,8 @@ export ALTERNATE_EDITOR='vim'
 export EDITOR="$EMACS_DEFAULT -nw"
 
 alias e="$EMACS_DEFAULT"
-alias en="$EMACS_DEFAULT -c & disown"
 alias enw="$EMACS_DEFAULT -nw"
+
+function en() {
+    $EMACS_DEFAULT -c $@ & disown
+}
