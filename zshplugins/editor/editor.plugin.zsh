bf9555a9d16e5c6d27146676f1891d3f9d5d01d5
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-08-25 17:54:14 +0000

    emacs: cleanup after fresh build.

--- zshplugins/editor/editor.plugin.zsh
+++ zshplugins/editor/editor.plugin.zsh
@@ -2,9 +2,9 @@ EMACS_DEFAULT="emacs_launcher.sh"
 export ALTERNATE_EDITOR='vim'
 export EDITOR="$EMACS_DEFAULT -nw"
 
+# Emacs Launchers
 alias e="$EMACS_DEFAULT"
 alias enw="$EMACS_DEFAULT -nw"
-
 function en() {
     $EMACS_DEFAULT -c $@ & disown
 }
