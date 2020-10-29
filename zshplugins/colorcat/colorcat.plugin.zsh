4aace93a964b34eb8985a22cd6bb15d6d39d876c
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-10-29 14:27:43 -0700

    bat: Now using [bat](https://github.com/sharkdp/bat).

--- zshplugins/colorcat/colorcat.plugin.zsh
+++ zshplugins/colorcat/colorcat.plugin.zsh
@@ -1,3 +1,9 @@
 # Colored Readers
-alias ccat='pygmentize -g'
-export LESSOPEN='| pygmentize -g %s'
+# alias ccat='pygmentize -g'
+# export LESSOPEN='| pygmentize -g %s'
+
+# I've switched to using [bat](https://github.com/sharkdp/bat):
+alias cat='bat'
+alias less='bat'
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export BAT_THEME="Solarized (light)"
