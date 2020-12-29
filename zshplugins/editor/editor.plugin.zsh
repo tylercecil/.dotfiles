39da09f876913dffb4df957ea0c60a4c4ac0873a
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-12-29 15:27:56 -0800

    kak: Adds kak-lsp

--- zshplugins/editor/editor.plugin.zsh
+++ zshplugins/editor/editor.plugin.zsh
@@ -3,8 +3,15 @@ export EDITOR="nvim"
 export ALTERNATE_EDITOR='vim'
 
 # Emacs Launchers
-alias e="$EMACS_DEFAULT"
-alias enw="$EMACS_DEFAULT -nw"
-function en() {
-    $EMACS_DEFAULT -c $@ & disown
+# alias e="$EMACS_DEFAULT"
+# alias e="nvim"
+# alias enw="$EMACS_DEFAULT -nw"
+# function en() {
+#     $EMACS_DEFAULT -c $@ & disown
+
+function kak_main() {
+    local kak_ses=main-session
+    kak -s $kak_ses $@ 2> /dev/null || kak -c $kak_ses $@
 }
+
+alias e=kak
