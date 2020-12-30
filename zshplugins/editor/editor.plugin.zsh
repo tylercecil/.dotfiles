c904001f1b87bd71aa88605f3c9948fb9085bd00
Tyler Cecil <tyler.i.cecil@gmail.com>
2020-12-30 10:23:04 -0800

    zsh:editor: Adds commands to change default editor on the fly.

--- zshplugins/editor/editor.plugin.zsh
+++ zshplugins/editor/editor.plugin.zsh
@@ -1,17 +1,27 @@
-EMACS_DEFAULT="emacs_launcher.sh"
-export EDITOR="kak"
-export ALTERNATE_EDITOR='vim'
-
-# Emacs Launchers
-# alias e="$EMACS_DEFAULT"
-# alias e="nvim"
-# alias enw="$EMACS_DEFAULT -nw"
-# function en() {
-#     $EMACS_DEFAULT -c $@ & disown
-
-function kak_main() {
-    local kak_ses=main-session
-    kak -s $kak_ses $@ 2> /dev/null || kak -c $kak_ses $@
+function editor_default_emacs() {
+    EMACS_DEFAULT="emacs_launcher.sh"
+    EDITOR="$EMACS_DEFAULT"
+    ALTERNATE_EDITOR="vim"
+    alias enw="$EMACS_DEFAULT -nw"
+    function en() {
+        $EMACS_DEFAULT -c $@ & disown
+    }
 }
 
-alias e=kak
+function editor_default_nvim() {
+    EDITOR="nvim"
+    ALTERNATE_EDITOR="vim"
+}
+
+function editor_default_kak() {
+    EDITOR="kak"
+    ALTERNATE_EDITOR="vim"
+}
+
+function e() {
+    $EDITOR $@
+}
+
+# Change this line to update the default editor.
+# Can also be changed on the fly.
+editor_default_kak
