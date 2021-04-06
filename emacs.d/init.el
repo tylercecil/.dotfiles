9923d53daa4ad1d07c2566d3a8336000703c0523
Tyler Cecil <tyler.i.cecil@gmail.com>
2021-04-06 11:56:37 -0700

    emacs: Add gui flags to init.el.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -36,10 +36,12 @@
   :config
   (spaceline-spacemacs-theme))
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
 
 (defun frame-init (frame)
   (select-frame frame)
