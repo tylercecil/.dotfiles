fdd210a8d50134aee357054958ce02d154cf830f
Tyler Cecil <tyler.i.cecil@gmail.com>
2022-11-10 10:07:04 -0800

    delta: Set delta pager to use "less --mouse".

---
+++ config/zsh/modules/delta/init.zsh
@@ -0,0 +1,6 @@
+# See [delta](https://dandavison.github.io/delta/introduction.html)
+
+# Set the pager to be less, with mouse input enabled
+# (I was having trouble with this. Not sure why it's working in `bat` and not
+# `delta` out of the box...)
+export DELTA_PAGER='/usr/bin/less -RFX --mouse --wheel-lines=3'
