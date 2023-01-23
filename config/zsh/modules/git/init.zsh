6dc35a16e31203af4fbef999251c8a44b64026d1
Tyler Cecil <tyler.i.cecil@gmail.com>
2023-01-23 12:22:17 -0800

    zsh:git: add aliast for "git branch".

--- config/zsh/modules/git/init.zsh
+++ config/zsh/modules/git/init.zsh
@@ -10,5 +10,6 @@ alias grm='git rm'             # git rm
 alias glg='git log'            # git log
 alias gst='git status'         # git status
 
+alias gb='git branch'          # git branch
 alias gp='git push'            # git push
 alias gpp='git pull'           # git pull
