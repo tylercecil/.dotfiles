93df862199a456745c072aa51e84a6f34ef3e75b
Tyler Cecil <tyler.i.cecil@gmail.com>
2023-01-30 16:35:12 -0800

    update_screens.sh: Add logging.
    At some point in time I realized that my update_screens.sh was being run
    at unexpected times, causing very unusual behavior with some transient
    video buffer. Turning on logging as a first step.

--- bin/update_screens.sh
+++ bin/update_screens.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
+function log() {
+  logger --stderr -- update_screens.sh: "$@"
+}
+
+log 'script started...'
 LID=$( rg closed /proc/acpi/button/lid/LID/state --no-line-number -o \
     || rg open   /proc/acpi/button/lid/LID/state --no-line-number -o)
 SCREENS=$(xrandr | rg ' connected' -c)
+log "LID=${LID} ; SCREENS=${SCREENS}"
 
 if [[ $LID == "open" ]]   && [[ $SCREENS == "1" ]]; then
+  log "Running laptop.sh..."
   /home/tyler/.screenlayout/laptop.sh
 fi
 if [[ $LID == "open" ]]   && [[ $SCREENS == "2" ]]; then
+  log "Running two-monitor.sh..."
   /home/tyler/.screenlayout/two-monitor.sh
 fi
 if [[ $LID == "closed" ]] && [[ $SCREENS == "2" ]]; then
+  log "Running clam.sh..."
   /home/tyler/.screenlayout/clam.sh
 fi
+
