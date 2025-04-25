f5dc5a1b46caab9e75534b0dfeb4107b0d32dec4
Tyler Cecil <tyler.i.cecil@gmail.com>
2025-04-25 11:26:02 -0700

    zsh: Tweak voltus zsh config.

--- config/zsh/modules/voltus/init.zsh
+++ config/zsh/modules/voltus/init.zsh
@@ -6,39 +6,41 @@ export PATH=$PATH:$VOLTUS/bin
 alias dbprod='thor -e prod db --name schedulerdb exec pgcli'
 alias dbdev='thor -e dev db --name schedulerdb exec pgcli'
 
+eval "$(direnv hook zsh)"
+
 # The following block was requested by our on boarding process. I'm... Actually
 # pretty sure I could remove most of it, but just going to leave it for the time
 # being.
-if [[ "${PYENV_ROOT:-""}" == "" ]]; then
-  export PYENV_ROOT=$HOME/.pyenv
-  export PATH=$PYENV_ROOT/bin:$PATH
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
-
-# src: https://unix.stackexchange.com/a/217629
-pathmunge() {
-  if [ -d "$1" ]; then
-    if ! echo "$PATH" | grep -Eq "(^|:)$1($|:)"; then
-      if [ "$2" = "after" ]; then
-        PATH="$PATH:$1"
-      else
-        PATH="$1:$PATH"
-      fi
-    fi
-  fi
-}
-
-eval $(go env | grep GOPATH)
-mkdir -p "$GOPATH/bin"
-
-binPaths=(
-  "$GOPATH/bin"
-)
-
-for dir in "${binPaths[@]}"; do
-  pathmunge "$dir"
-done
-
-
+# if [[ "${PYENV_ROOT:-""}" == "" ]]; then
+#   export PYENV_ROOT=$HOME/.pyenv
+#   export PATH=$PYENV_ROOT/bin:$PATH
+#   eval "$(pyenv init --path)"
+#   eval "$(pyenv init -)"
+#   eval "$(pyenv virtualenv-init -)"
+# fi
+# 
+# # src: https://unix.stackexchange.com/a/217629
+# pathmunge() {
+#   if [ -d "$1" ]; then
+#     if ! echo "$PATH" | grep -Eq "(^|:)$1($|:)"; then
+#       if [ "$2" = "after" ]; then
+#         PATH="$PATH:$1"
+#       else
+#         PATH="$1:$PATH"
+#       fi
+#     fi
+#   fi
+# }
+# 
+# eval $(go env | grep GOPATH)
+# mkdir -p "$GOPATH/bin"
+# 
+# binPaths=(
+#   "$GOPATH/bin"
+# )
+# 
+# for dir in "${binPaths[@]}"; do
+#   pathmunge "$dir"
+# done
+# 
+# 
