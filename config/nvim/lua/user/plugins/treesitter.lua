a272600d5902914d5fd3f9f082776602fb45dba2
Tyler Cecil <tyler.i.cecil@gmail.com>
2025-10-01 15:49:13 -0700

    nvim: Add treesitter text-objects.

--- config/nvim/lua/user/plugins/treesitter.lua
+++ config/nvim/lua/user/plugins/treesitter.lua
@@ -2,6 +2,12 @@ return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs", -- Sets main module to use for opts
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		-- See https://github.com/LazyVim/LazyVim/issues/282#issuecomment-1433300058 for
+		-- more info...
+		init = function() end,
+	},
 	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 	opts = {
 		ensure_installed = {
@@ -33,12 +39,59 @@ return { -- Highlight, edit, and navigate code
 			-- disable = { "ruby" },
 		},
 		incremental_selection = {
-			enabled = true,
+			enable = true,
 			keymaps = {
-				init_selection = "<leader>i",
-				node_incremental = "<leader>i",
-				scope_incremental = "<leader>k",
-				node_decremental = "<leader>j",
+				init_selection = "<leader>j",
+				node_incremental = "<leader>j",
+				scope_incremental = false,
+				node_decremental = "<leader>k",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["if"] = { query = "@function.inner", desc = "[i]nside [f]unction" },
+					["af"] = { query = "@function.outer", desc = "[a]round [f]unction" },
+
+					["ia"] = { query = "@parameter.inner", desc = "[i]nside [a]arg" },
+					["aa"] = { query = "@parameter.outer", desc = "[a]round [a]arg" },
+
+					["it"] = { query = "@class.inner", desc = "[i]nside [t]ype" },
+					["at"] = { query = "@class.outer", desc = "[a]round [t]ype" },
+
+					["ic"] = { query = "@call.inner", desc = "[i]nside [c]all" },
+					["ac"] = { query = "@call.outer", desc = "[a]round [c]all" },
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true,
+				goto_next_start = {
+					["]f"] = { query = "@function.outer", desc = "next [f]unction" },
+					["]a"] = { query = "@parameter.inner", desc = "next [a]arg" },
+					["]t"] = { query = "@class.outer", desc = "next [t]ype" },
+					["]c"] = { query = "@call.outer", desc = "next [c]all" },
+				},
+				goto_next_end = {
+					["]F"] = { query = "@function.outer", desc = "next [F]unction" },
+					["]A"] = { query = "@parameter.inner", desc = "next [A]arg" },
+					["]T"] = { query = "@class.outer", desc = "next [T]ype" },
+					["]C"] = { query = "@call.outer", desc = "next [C]all" },
+				},
+				goto_previous_start = {
+					["[f"] = { query = "@function.outer", desc = "prev [f]unction" },
+					["[a"] = { query = "@parameter.inner", desc = "prev [a]arg" },
+					["[t"] = { query = "@class.outer", desc = "prev [t]ype" },
+					["[c"] = { query = "@call.outer", desc = "prev [c]all" },
+				},
+				goto_previous_end = {
+					["[F"] = { query = "@function.outer", desc = "prev [F]unction" },
+					["[A"] = { query = "@parameter.inner", desc = "prev [A]arg" },
+					["[T"] = { query = "@class.outer", desc = "prev [T]ype" },
+					["[C"] = { query = "@call.outer", desc = "prev [C]all" },
+				},
 			},
 		},
 	},
