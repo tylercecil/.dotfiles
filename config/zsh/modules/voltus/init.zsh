2a188a83faa79a3d9999e0ec23c63c16970730fa
Tyler Cecil <tyler.i.cecil@gmail.com>
2026-01-05 13:35:18 -0800

    zsh:voluts: voltus database.

--- config/zsh/modules/voltus/init.zsh
+++ config/zsh/modules/voltus/init.zsh
@@ -1,10 +1,42 @@
 export VOLTUS=$HOME/src/voltus
 export PATH=$PATH:$VOLTUS/bin
 
-# I want to make this a more general function, but for now I've got two dumb
-# ones...
-alias dbprod='thor -e prod db --name schedulerdb exec pgcli'
-alias dbdev='thor -e dev db --name schedulerdb exec pgcli'
+function db() {
+	dbname=scheduler
+	environment=dev
+	while getopts "e:d:h" opt; do
+		case ${opt} in
+			e) environment=${OPTARG} ;;
+			d) dbname=${OPTARG} ;;
+			h|?) printf "Usage: %s: [-e env] [-d db_name]\n" $0 ;;
+		esac
+	done
+
+	echo -e "\e[1mdb_name:\e[0m" ${dbname} "\t" "\e[1menv:\e[0m" ${environment}
+	case ${dbname} in
+		s|sched|scheduler)
+			dbname='schedulerdb'
+			;;
+		va|vaf|voltapp|voltapp_postgres)
+			dbname="voltapp"
+			;;
+		mm|marketmirror)
+			dbname='static-readwrite-marketmirror'
+	esac
+
+	case ${dbname} in
+		static-readwrite-marketmirror)
+			PGDATABASE=postgres thor db -e ${environment} exec -c direct -n ${dbname} pgcli
+			;;
+		*) printf "Unknown db_name %s\n" ${dbname}
+			thor -e ${environment} db --name ${dbname} exec pgcli
+			;;
+	esac
+}
+
+alias dbprod='db -e prod'
+alias dbdev='db -e dev'
+
 
 eval "$(direnv hook zsh)"
 
